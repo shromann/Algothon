b7ab263a38eab35401a4cf6d7281ad911d8a8607
Shromann Majumder <shromannmajumder@gmail.com>
2022-06-29 11:00:52 +1000

    inital files

---
+++ BigManTingYe.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import regex as re
+
+def getMyPosition(prices):
+    
+    position = np.array()
+    
+    
+
+    return position
---
+++ PLAN.md
@@ -0,0 +1,9 @@
+# Judging Criteria
+
+## Technical
+	- Profit & Loss
+	- Total Portfolio Return
+	- Sharpe Ratio
+## Presentation
+	- Clarity
+	- Maturity
